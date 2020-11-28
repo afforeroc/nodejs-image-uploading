bb2354b3d2243d6ae72b8eebf41d6d8fbaaf6037
Andrés Forero <afforeroc@unal.edu.co>
2020-11-27 23:10:43 -0500

    Minor space

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
     filename: function(req, file, cb){
       cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
-  });
+});
 
 // Init Upload
 const upload = multer({
